db9922d5a64819e8874eb7d225fc20231fe5f508
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-04-26 18:36:56 -0700

    changed api path to db module and created app.use for the api

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require('path');
-const api = require ('./public/assets/js/index.js');
+const api = require ('./db/db.json');
 
 const PORT = process.env.port || 3001;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/api', api);
 
 app.use(express.static('public'));
 
@@ -14,5 +17,10 @@ app.get('/notes', (req, res) =>
     );
 
 app.get('*', (req,res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html')))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+    );
+
+    app.listen(PORT, () =>
+        console.log(`App listening at http://localhost:${PORT}`)
+        );
 
