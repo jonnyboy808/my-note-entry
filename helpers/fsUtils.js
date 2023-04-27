7720165e4462e364e6405dc948bc1f62e5f238ea
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-04-26 19:02:53 -0700

    corrected fsUtils file that was producting an error

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -4,9 +4,21 @@ const util = require('util');
 const readFromFile = util.promisify(fs.readFile);
 
 const writeToFile = (destination, content) =>
-fs.writeFile(destination, JSON.stringify(content, null, 4) (err) =>
-err ? console.log(err) : console.info(`\n Data written to ${destination}`)
+fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+err ? console.error(err) : console.info(`\n Data written to ${destination}`)
 );
+
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+    } else {
+        const parsedData = JSON.parse(data);
+        parsedData.push(content);
+        writeToFile(file, parsedData);
+    }
+});
+};
 
 
 
