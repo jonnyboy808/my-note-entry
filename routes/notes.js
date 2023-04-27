66a495474dfa4ad4f8a257df473e078cab60eeaf
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-04-26 22:04:01 -0700

    added additional code to notesjs to support deleting a note

--- routes/notes.js
+++ routes/notes.js
@@ -1,2 +1,35 @@
 const api = require('express').Router();
-const { readFromFile, }
+const { readFromFile, readAndAppend, readFilterAppend, readFilterAndAppend } = require('../helpers/fsUtils');
+
+const { v4: uuidv4 } = require('uuid');
+
+api.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+api.post('/', (req, res) => {
+    const {title, text } = req.body;
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json('Note added. ');
+    } else {
+        res.error('Error adding note. ');
+    }
+});
+
+api.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    if (id) {
+        readFilterAndAppend('./db/db.json', id);
+        res.json('Notes deleted. ');
+        res.error('Error deleting note. ');
+    }
+});
+
+module.exports = api;
