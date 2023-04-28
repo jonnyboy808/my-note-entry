1745d30047fe50c9a5619d44f22de144ebc784ea
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-04-27 17:40:49 -0700

    corrected port misspell

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const api = require ('./routes/index');
 
 // creates an active port at 3001
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
